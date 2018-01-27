84e68b1beb282a55e2fb7bcf4bcdf647b16d9b47
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-01-27 13:48:35 +0100

    Convert physics to templated class

--- include/Physics.hpp
+++ include/Physics.hpp
@@ -4,8 +4,12 @@
 #include <numeric>
 #include "claws/Vect.hpp"
 
-namespace physics
+template<class Solver>
+struct Physics
 {
+  Solver &solver;
+
+private:
   using expander = int[];
 
   template<class First, class... It>
@@ -31,31 +35,30 @@ namespace physics
       });
   }
 
-  template<class... It, class Dest>
-  void classicSolve(std::tuple<It...> begin, std::tuple<It...> end, Dest &&dest)
+  template<class... It>
+  void classicSolve(std::tuple<It...> begin, std::tuple<It...> end)
   {
-    auto checkOthers([](auto begin, auto end, std::tuple<It...> otherBegins, std::tuple<It...> otherEnds, Dest &&dest)
+    auto checkOthers([this](auto begin, auto end, std::tuple<It...> otherBegins, std::tuple<It...> otherEnds)
 		     {
-		       auto check([](auto begin1, auto end1, auto begin2, auto end2, Dest &&dest)
+		       auto check([this](auto begin1, auto end1, auto begin2, auto end2)
 				  {
 				    for (auto it1(begin1); it1 < end1; ++it1)
 				      for (auto it2(begin2); it2 < end2; ++it2)
-					dest(*it1, *it2);
+					solver(*it1, *it2);
 				  });
 		       (void)expander{check(begin, end, std::get<It>(otherBegins), std::get<It>(otherEnds))...};
 		     });
-    (void)expander{checkOthers(std::get<It>(begin), std::get<It>(end), {std::get<It>(begin)...},  {std::get<It>(end)...}, dest)...};
+    (void)expander{checkOthers(std::get<It>(begin), std::get<It>(end), {std::get<It>(begin)...},  {std::get<It>(end)...})...};
   }
 
-
-  template<class... It, class Dest>
-  void checkCollisionImpl(std::tuple<It...> begin, std::tuple<It...> end, Dest &&dest)
+  template<class... It>
+  void checkCollisionImpl(std::tuple<It...> begin, std::tuple<It...> end)
   {
     auto [min, max] = getMinMaxImpl(begin, end);
     claws::Vect<2u, double> mid((min + max) * 0.5);
 
     if (claws::Vect<2u, std::size_t>{(std::get<It>(begin) - std::get<It>(end))...}.sum() < 20)
-      return classicSolve(begin, end, dest);
+      return classicSolve(begin, end);
     for (std::size_t i(0ul); i != 2ul; ++i)
       {
 	auto isBelow([mid, i](auto const &a){
@@ -65,25 +68,25 @@ namespace physics
 	      return a->pos[i] - a->radius < a->mid[i];
 	  });
 	for (auto pred : {isBelow, isAbove})
-	  checkCollisionImpl(begin, {std::partition(std::get<It>(begin), std::get<It>(end), pred)...}, dest);
+	  checkCollisionImpl(begin, {std::partition(std::get<It>(begin), std::get<It>(end), pred)...});
       }
   }
 
-  template<class... T, class Dest>
-  void prepareCollisionCheck(std::tuple<T...> begin, std::tuple<T...> end, Dest &&dest)
+public:
+  template<class... T>
+  void prepareCollisionCheck(std::tuple<T...> begin, std::tuple<T...> end)
   {
     std::tuple<std::vector<decltype(&*std::get<T>(begin))>...> ptrStorage;
 
     (void)expander{(std::get<std::vector<decltype(&*std::get<T>(begin))>>(ptrStorage).resize(std::get<T>(end) - std::get<T>(begin)), 0)...};
     (void)expander{(std::transform(std::get<T>(begin), std::get<T>(end), std::get<std::vector<decltype(&*std::get<T>(begin))>>(ptrStorage).begin(), [](auto &a){return &a;}), 0)...};
     checkCollisionImpl({std::get<std::vector<decltype(&*std::get<T>(begin))>>(ptrStorage).begin()...},
-		       {std::get<std::vector<decltype(&*std::get<T>(begin))>>(ptrStorage).end()...},
-		       dest);
+		       {std::get<std::vector<decltype(&*std::get<T>(begin))>>(ptrStorage).end()...});
   }
 
-  template<class It1, class It2, class Dest>
-  void checkCollision(It1 begin1, It1 end1, It2 begin2, It2 end2, Dest &&dest)
+  template<class It1, class It2>
+  void checkCollision(It1 begin1, It1 end1, It2 begin2, It2 end2)
   {
-    checkCollisionImpl({begin1, begin2}, {end1, end2}, dest);
+    checkCollisionImpl({begin1, begin2}, {end1, end2});
   }
 };
