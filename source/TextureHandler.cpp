3e4cea35097d4abddb3909a7ae5b4f8f8893c43d
David Zeng <david.zeng@epitech.eu>
2018-01-27 13:28:24 +0100

    changed singleton texture handler to instance in display

--- source/TextureHandler.cpp
+++ source/TextureHandler.cpp
@@ -1,67 +1,50 @@
 #include "TextureHandler.hpp"
 
-std::unique_ptr<TextureHandler> TextureHandler::_instance(nullptr);
-
 TextureHandler::TextureHandler()
 {
-}
-
-void TextureHandler::initTextureHandler()
-{
-  _instance.reset(new TextureHandler());
-  _instance->addTexture(TextureList::BOYAUX, "resources/boyaux.bmp");
   // Already loaded in display.cpp
   // _instance->addTexture(PLANET, "resources/planet.bmp");
-  _instance->addTexture(TextureList::SWORDRADIUS, "resources/swordRadius.bmp");
-  _instance->addTexture(TextureList::BULLET, "resources/bite.bmp");
-  _instance->addTexture(TextureList::ZOMBIE, "resources/ZombieSpriteSheet.bmp");
-  _instance->addTexture(TextureList::PLAYER, "resources/PlayerSpriteSheet.bmp");
-  _instance->addTexture(TextureList::HUMAN, "resources/CitizenSpriteSheet.bmp");
-  _instance->addTexture(TextureList::GRASS, "resources/grass.bmp");
-  _instance->addTexture(TextureList::NONE, "resources/grass.bmp");
-  _instance->addTexture(TextureList::HOUSE1, "resources/Shed.bmp");
-  _instance->addTexture(TextureList::HOUSE2, "resources/House.bmp");
-  _instance->addTexture(TextureList::HOUSE3, "resources/Mansion.bmp");
-  _instance->addTexture(TextureList::BORDER, "resources/Border.bmp");
-  _instance->addTexture(TextureList::TEST, "resources/test.bmp");
-  _instance->addTexture(TextureList::BOMB, "resources/Bomb.bmp");
-  _instance->addTexture(TextureList::BOMBHOLLOW, "resources/BombHollow.bmp");
-  _instance->addTexture(TextureList::SLASH, "resources/SlashSpriteSheet.bmp");
-  _instance->addTexture(TextureList::SLASH2, "resources/Slash2SpriteSheet.bmp");
-  _instance->addTexture(TextureList::SHURIKEN, "resources/ShurikenSpriteSheet.bmp");
-  _instance->addTexture(TextureList::BARFRONT, "resources/BarFront.bmp");
-  _instance->addTexture(TextureList::BARBACK, "resources/BarBack.bmp");
-  _instance->addTexture(TextureList::SPIN, "resources/Spin.bmp");
-  _instance->addTexture(TextureList::SPINHOLLOW, "resources/SpinHollow.bmp");
-  _instance->addTexture(TextureList::BOMB_SPRITE, "resources/BombSpriteSheet.bmp");
-  _instance->addTexture(TextureList::EXPLOSION, "resources/ExplosionSpriteSheet.bmp");
-  _instance->addTexture(TextureList::ARROW, "resources/arrow.bmp");
-  _instance->addTexture(TextureList::BLOOD, "resources/BloodSplaterSpriteSheet.bmp");
-  _instance->addTexture(TextureList::TUTO, "resources/Tuto.bmp");
-  _instance->addTexture(TextureList::STARTPAGE, "resources/StartPage.bmp");
-  _instance->addTexture(TextureList::FLESH_SPRITE, "resources/GutsSpriteSheet.bmp");
-  _instance->addTexture(TextureList::ZOMBIEHEAD, "resources/ZombieHead.bmp");
-  _instance->addTexture(TextureList::HUMANHEAD, "resources/CitizenHead.bmp");
-  _instance->addTexture(TextureList::SAVE_ME, "resources/SaveMe.bmp");
-  _instance->addTexture(TextureList::HIGH_FIVE, "resources/HighFive.bmp");
-}
-
-TextureHandler& TextureHandler::getInstance()
-{
-  return *_instance;
-}
-
-void TextureHandler::destroyTextureHandler()
-{
-  _instance.reset(nullptr);
+  addTexture(TextureList::BOYAUX, "resources/boyaux.bmp");
+  addTexture(TextureList::SWORDRADIUS, "resources/swordRadius.bmp");
+  addTexture(TextureList::BULLET, "resources/bite.bmp");
+  addTexture(TextureList::ZOMBIE, "resources/ZombieSpriteSheet.bmp");
+  addTexture(TextureList::PLAYER, "resources/PlayerSpriteSheet.bmp");
+  addTexture(TextureList::HUMAN, "resources/CitizenSpriteSheet.bmp");
+  addTexture(TextureList::GRASS, "resources/grass.bmp");
+  addTexture(TextureList::NONE, "resources/grass.bmp");
+  addTexture(TextureList::HOUSE1, "resources/Shed.bmp");
+  addTexture(TextureList::HOUSE2, "resources/House.bmp");
+  addTexture(TextureList::HOUSE3, "resources/Mansion.bmp");
+  addTexture(TextureList::BORDER, "resources/Border.bmp");
+  addTexture(TextureList::TEST, "resources/test.bmp");
+  addTexture(TextureList::BOMB, "resources/Bomb.bmp");
+  addTexture(TextureList::BOMBHOLLOW, "resources/BombHollow.bmp");
+  addTexture(TextureList::SLASH, "resources/SlashSpriteSheet.bmp");
+  addTexture(TextureList::SLASH2, "resources/Slash2SpriteSheet.bmp");
+  addTexture(TextureList::SHURIKEN, "resources/ShurikenSpriteSheet.bmp");
+  addTexture(TextureList::BARFRONT, "resources/BarFront.bmp");
+  addTexture(TextureList::BARBACK, "resources/BarBack.bmp");
+  addTexture(TextureList::SPIN, "resources/Spin.bmp");
+  addTexture(TextureList::SPINHOLLOW, "resources/SpinHollow.bmp");
+  addTexture(TextureList::BOMB_SPRITE, "resources/BombSpriteSheet.bmp");
+  addTexture(TextureList::EXPLOSION, "resources/ExplosionSpriteSheet.bmp");
+  addTexture(TextureList::ARROW, "resources/arrow.bmp");
+  addTexture(TextureList::BLOOD, "resources/BloodSplaterSpriteSheet.bmp");
+  addTexture(TextureList::TUTO, "resources/Tuto.bmp");
+  addTexture(TextureList::STARTPAGE, "resources/StartPage.bmp");
+  addTexture(TextureList::FLESH_SPRITE, "resources/GutsSpriteSheet.bmp");
+  addTexture(TextureList::ZOMBIEHEAD, "resources/ZombieHead.bmp");
+  addTexture(TextureList::HUMANHEAD, "resources/CitizenHead.bmp");
+  addTexture(TextureList::SAVE_ME, "resources/SaveMe.bmp");
+  addTexture(TextureList::HIGH_FIVE, "resources/HighFive.bmp");
 }
 
 void TextureHandler::addTexture(TextureList id, std::string const& path)
 {
-  _instance->_textures[id] = my_opengl::loadTexture(path);
+  _textures[id] = my_opengl::loadTexture(path);
 }
 
 Texture TextureHandler::getTexture(TextureList id) const
 {
-  return (_instance->_textures.at(id));
+  return (_textures.at(id));
 }
